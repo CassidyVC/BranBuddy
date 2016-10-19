2c18536bb72ab6ba1be8ddbbb7609107717354b9
Kenializ <kenia18@brandeis.edu>
2016-10-19 15:16:32 -0400

    stuff

--- pages/reserve/client/reserve.js
+++ pages/reserve/client/reserve.js
@@ -1,9 +1,20 @@
-const ourSponsors = [
- {name:"Google",amt:"$50 million"},
- {name:"Apple",amt:"$20 million"},
- {name:"Facebook",amt:"$40 million"},
-]
+Template.reserve.events({
+"click #submitreserve": function(event){
+	
+	console.log("hey we clicked it");
+	alert("yay!");
+	const name = $("#name").val();
+	const age = $("#age").val();
+    const location = $("#location").val();
+    const destination = $("destination").val();
+    const time = $("time").val();
+	const date = new Date();
+	const jsonobj = {name:name,age:age,location:location,destination:destination,time:time,date:date};
+	Reserve.insert(jsonobj);
+}
+})
+
 
 Template.reserve.helpers({
-	sponsorData: ourSponsors
+
 })
