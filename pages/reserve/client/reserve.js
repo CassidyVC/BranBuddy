631c0c21c5180a1bf0ad278cbead89e67c9d943c
Kenializ <kenia18@brandeis.edu>
2016-10-31 15:11:07 -0400

    reserve

--- pages/reserve/client/reserve.js
+++ pages/reserve/client/reserve.js
@@ -8,8 +8,10 @@ Template.reserve.events({
     const location = $("#location").val();
     const destination = $("#destination").val();
     const time = $("#time").val();
+    const.cellphone = $("#cellphone").val();
+    const.email = $("#email").val();
 	const date = new Date();
-	const jsonobj = {name:name,age:age,location:location,destination:destination,time:time,date:date};
+	const jsonobj = {name:name,age:age,location:location,destination:destination,time:time,cellphone:cellphone,email:email,date:date};
 	Reserve.insert(jsonobj);
 }
 })
