f315574ef76ce353c9a66345442306a0dca4d587
CassidyVC <cassidyvc@brandeis.edu>
2016-11-21 15:03:51 -0500

    Comments

--- pages/comments/client/comments.js
+++ pages/comments/client/comments.js
@@ -1,12 +1,17 @@
 Template.comments.events({
-"click #submitcomment": function(event){
+"click #submitComment": function(event){
 
 	const comment = $("#comment").val();
 	const date = new Date();
 	const jsonobj = {comment:comment, date:date};
 	Comments.insert(jsonobj);
-}
+},
+'click .deleteRequest':function(event) {
+	console.log(event.target);
+	Comments.remove({_id:this._id});
+	}
 })
+
 
 
 Template.comments.helpers({
